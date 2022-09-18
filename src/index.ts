d3d4a9f46ab99834957338606cddf88a7a5549db
aaronb97 <aaron9becker7@gmail.com>
2022-09-17 20:24:14 -0400

    set up firebase admin

--- src/index.ts
+++ src/index.ts
@@ -3,9 +3,17 @@ const { Pool, Client } = pg;
 import express from "express";
 import { MikroORM } from "@mikro-orm/core";
 import mikroOrmConfig from "./mikro-orm.config";
+import cors from "cors";
+import { applicationDefault, initializeApp } from "firebase-admin/app";
+import admin from "firebase-admin";
+
 const app = express();
 
 const port = 3000;
+
+const firebaseApp = admin.initializeApp({
+  credential: applicationDefault(),
+});
 
 const initOrm = async () => {
   const orm = await MikroORM.init(mikroOrmConfig);
@@ -23,6 +31,56 @@ const credentials = {
 };
 
 const pool = new Pool(credentials);
+
+app.use(cors({ origin: "http://localhost:1234" }));
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+app.use((req, res, next) => {
+  // to allow cross domain requests to send cookie information.
+  res.header("Access-Control-Allow-Credentials", "true");
+
+  // origin can not be '*' when crendentials are enabled. so need to set it to the request origin
+  res.header("Access-Control-Allow-Origin", req.headers.origin);
+
+  // list of methods that are supported by the server
+  res.header("Access-Control-Allow-Methods", "OPTIONS,GET,PUT,POST,DELETE");
+
+  res.header(
+    "Access-Control-Allow-Headers",
+    "X-Requested-With, X-HTTP-Method-Override, Content-Type, Accept, X-XSRF-TOKEN"
+  );
+
+  next();
+});
+
+app.post("/login", async (req, res) => {
+  const { authorization } = req.headers;
+  if (!authorization) {
+    res.status(400);
+    res.json("Must specifiy auth token");
+    return;
+  }
+
+  if (!authorization.startsWith("Bearer ")) {
+    res.status(400);
+    res.json("Invalid token");
+    return;
+  }
+
+  try {
+    const decodedToken = await firebaseApp
+      .auth()
+      .verifyIdToken(authorization.substring(7, authorization.length));
+    console.log(decodedToken);
+    res.status(200);
+    res.json("Successfully logged in");
+  } catch (e) {
+    console.error(e);
+    res.status(400);
+    res.json("Invalid token");
+  }
+});
 
 app.get("/planets", async (req, res) => {
   const result = await pool.query("SELECT * from planets");
