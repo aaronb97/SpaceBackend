746804ae1fdb90bd84344f945abb78bfde37fd56
aaronb97 <aaron9becker7@gmail.com>
2022-09-18 20:38:45 -0400

    reduce significant figures

--- src/planets.ts
+++ src/planets.ts
@@ -1,18 +1,11 @@
 import { PlanetType } from "./entities/Planet";
 
 export const planets: PlanetPrototype[] = [
-  [["Earth", 1.497184002134871e8, -1.372687435345056e7, -4.114995952909812e2]],
+  [["Earth", 1.4971840021e8, -1.3726874353e7, -4.1149959529e2]],
   [["The Sun", 0, 0, 0], { type: "star" }],
-  [["Mars", 1.838306119805684e8, 1.103036925440678e8, -2.197524413727023e6]],
-  [["Venus", -9.540214119813448e7, 4.92174537581663e7, 6.180586806831401e6]],
-  [
-    [
-      "Mercury",
-      5.264779706136116e7,
-      -2.266129945726145e7,
-      -6.681062812312682e6,
-    ],
-  ],
+  [["Mars", 1.8383061198e8, 1.1030369254e8, -2.1975244137e6]],
+  [["Venus", -9.5402141198e7, 4.9217453758e7, 6.1805868068e6]],
+  [["Mercury", 5.2647797061e7, -2.2661299457e7, -6.6810628123e6]],
 ];
 
 interface Options {
@@ -20,3 +13,5 @@ interface Options {
 }
 
 type PlanetPrototype = [[string, number, number, number], Options?];
+
+//regex to limit to 10 sig figs: (?<=\d\.\d{10})\d+
