370a735d967f7b5d755db9a27e50186cb5309dd0
aaronb97 <aaron9becker7@gmail.com>
2022-09-22 08:26:34 -0400

    update config

--- src/mikro-orm.config.ts
+++ src/mikro-orm.config.ts
@@ -1,18 +1,19 @@
-import { isProd } from "./constants";
-import { User } from "./entities/User";
-import path from "path";
-import { MikroORM } from "@mikro-orm/core";
-import { TsMorphMetadataProvider } from "@mikro-orm/reflection";
-import { Planet } from "./entities/Planet";
+import { isProd } from './constants';
+import { User } from './entities/User';
+import path from 'path';
+import { MikroORM } from '@mikro-orm/core';
+import { TsMorphMetadataProvider } from '@mikro-orm/reflection';
+import { Planet } from './entities/Planet';
 
-export default {
-  dbName: "space",
-  type: "postgresql" as const,
+const config: Parameters<typeof MikroORM.init>[0] = {
+  dbName: 'space',
+  type: 'postgresql' as const,
   debug: !isProd,
   entities: [User, Planet],
   metadataProvider: TsMorphMetadataProvider,
   migrations: {
-    path: path.join(__dirname, "./migrations"),
-    pattern: /^[\w-]+\d+\.[tj]s$/,
+    path: path.join(__dirname, './migrations'),
   },
-} as Parameters<typeof MikroORM.init>[0];
+};
+
+export default config;
