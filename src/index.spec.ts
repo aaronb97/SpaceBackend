60a6ef95b0a29e1f5ac5c7fe4a267299e317fe2a
aaronb97 <aaron9becker7@gmail.com>
2022-09-22 20:24:37 -0400

    add test for creating user

--- src/index.spec.ts
+++ src/index.spec.ts
@@ -5,6 +5,16 @@ import * as http from 'http';
 import { Express } from 'express-serve-static-core';
 import { MikroORM, IDatabaseDriver, Connection } from '@mikro-orm/core';
 import mikroOrmConfig from './mikro-orm.config';
+import { validateUser } from './validateUser';
+import { DecodedIdToken } from 'firebase-admin/lib/auth/token-verifier';
+
+jest.mock('./validateUser');
+
+jest.mocked(validateUser).mockImplementation(
+  async (jwt: string | undefined) =>
+    // eslint-disable-next-line @typescript-eslint/consistent-type-assertions
+    await Promise.resolve({ uid: jwt } as DecodedIdToken),
+);
 
 let connection: http.Server;
 let expressApp: Express;
@@ -59,5 +69,26 @@ describe('/planets', () => {
 
     expect(data.some((planet: { name: string }) => planet.name === 'Earth'));
     expect(data.some((planet: { name: string }) => planet.name === 'The Sun'));
+  });
+});
+
+describe('/login', () => {
+  test('it should create a new user when logging in with a new user', async () => {
+    const result = await axiosClient.post('/login', undefined, {
+      headers: {
+        authorization: 'user1',
+      },
+    });
+
+    expect(result.data.planet.name).toBe('Earth');
+    expect(result.status).toBe(201);
+
+    const result2 = await axiosClient.post('/login', undefined, {
+      headers: {
+        authorization: 'user1',
+      },
+    });
+
+    expect(result2.status).toBe(200);
   });
 });
