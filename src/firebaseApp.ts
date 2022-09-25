6f52c24374a8662e00684d17a2a890449802d558
aaronb97 <aaron9becker7@gmail.com>
2022-09-25 09:57:27 -0400

    firebase environment variables

--- src/firebaseApp.ts
+++ src/firebaseApp.ts
@@ -1,6 +1,9 @@
-import { applicationDefault } from "firebase-admin/app";
-import admin from "firebase-admin";
+import admin from 'firebase-admin';
 
 export const firebaseApp = admin.initializeApp({
-  credential: applicationDefault(),
+  credential: admin.credential.cert({
+    projectId: process.env.FIREBASE_PROJECT_ID,
+    privateKey: process.env.FIREBASE_PRIVATE_KEY,
+    clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
+  }),
 });
