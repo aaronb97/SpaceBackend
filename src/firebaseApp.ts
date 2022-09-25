52a1d8fd2d1f547da2d3a86bd8c0150426bd04bc
aaronb97 <aaron9becker7@gmail.com>
2022-09-25 10:06:04 -0400

    parse private key

--- src/firebaseApp.ts
+++ src/firebaseApp.ts
@@ -3,7 +3,7 @@ import admin from 'firebase-admin';
 export const firebaseApp = admin.initializeApp({
   credential: admin.credential.cert({
     projectId: process.env.FIREBASE_PROJECT_ID,
-    privateKey: process.env.FIREBASE_PRIVATE_KEY,
+    privateKey: JSON.parse(process.env.FIREBASE_PRIVATE_KEY ?? ''),
     clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
   }),
 });
