a5a53e6d40083b0e4a3648d8e5afe7ef82bb1397
aaronb97 <aaron9becker7@gmail.com>
2022-09-25 09:47:57 -0400

    use env PORT

--- src/index.ts
+++ src/index.ts
@@ -13,9 +13,9 @@ const main = async () => {
 
   await defineRoutes(app, orm);
 
-  const port = 3000;
+  const port = process.env.PORT ?? 3000;
   app.listen(port, () => {
-    console.log(`Example app listening on port ${port}`);
+    console.log(`Listening on port ${port}`);
   });
 
   process.on('exit', () => {
