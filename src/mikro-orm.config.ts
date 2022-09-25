15f4bae62d5b422bd5bac58506148848823f4c2d
aaronb97 <aaron9becker7@gmail.com>
2022-09-25 08:44:09 -0400

    Add starting in prod mode

--- src/mikro-orm.config.ts
+++ src/mikro-orm.config.ts
@@ -1,17 +1,14 @@
 import { isProd } from './constants';
-import { User } from './entities/User';
 import path from 'path';
 import { MikroORM } from '@mikro-orm/core';
 import { TsMorphMetadataProvider } from '@mikro-orm/reflection';
-import { Planet } from './entities/Planet';
-import { Username } from './entities/Username';
-import { Item } from './entities/Item';
 
 const config: Parameters<typeof MikroORM.init>[0] = {
   dbName: 'space',
   type: 'postgresql' as const,
   debug: !isProd,
-  entities: [User, Planet, Username, Item],
+  entities: ['./dist/entities'],
+  entitiesTs: ['./src/entities'],
   metadataProvider: TsMorphMetadataProvider,
   migrations: {
     path: path.join(__dirname, './migrations'),
